82dc60df34344086dc24f19fca79f61058b8bc9b
Aya Sulyman <ayasulyiman@gmail.com>
2026-07-09 16:30:51 +0300

    Backend Config

---
+++ Backend/src/config/database.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const connectDatabase = async () => {
+    try {
+
+        await mongoose.connect(
+            process.env.MONGO_URI as string
+        );
+
+        console.log("MongoDB Connected Successfully");
+
+    } catch (error) {
+
+        console.error(
+            "MongoDB Connection Error:",
+            error
+        );
+
+        process.exit(1);
+    }
+};
+
+export default connectDatabase;
