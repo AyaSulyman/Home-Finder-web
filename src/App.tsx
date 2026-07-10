4193cf33650e2f92406b54df60099c4462ad612f
Mohammad Ahmad <mhmd.ahmad_04@outlook.com>
2026-07-10 04:59:44 +0300

    Add login and signup UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+
 import { Routes, Route } from "react-router-dom";
+
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
 
 import BrowseProperties from "./browse-properties/BrowseProperties";
 import PropertyDetails from "./property-details/PropertyDetails";
@@ -9,6 +13,8 @@ const App: React.FC = () => {
     <Routes>
       <Route path="/browse-properties" element={<BrowseProperties />} />
       <Route path="/property/:id" element={<PropertyDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 };
